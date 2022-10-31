751882c709bc43ffb90fd5bdfef070b701fb004d
Yeonoc <chlqhtjr752@kookmin.ac.kr>
2022-10-31 16:31:19 +0900

    2번과제 Rigid 변수 추가

---
+++ SampleScene/Assets/Scripts/CubeRigid.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeRigid : MonoBehaviour
+{
+    public Rigidbody myRigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody.useGravity = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
